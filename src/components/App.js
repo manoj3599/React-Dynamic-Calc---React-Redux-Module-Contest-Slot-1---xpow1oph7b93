02f42cb4858b0f679a940ab7fc78e54912f5ce19
manoj nayak <115462742+manoj3599@users.noreply.github.com>
2023-12-17 09:47:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
+
 const App = () => {
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    // Update the result whenever input1 or input2 changes
+    setResult(Number(input1) + Number(input2));
+  }, [input1, input2]);
+
+  const handleInput1Change = (e) => {
+    setInput1(e.target.value);
+  };
+
+  const handleInput2Change = (e) => {
+    setInput2(e.target.value);
+  };
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' type='number' value={input1} onChange={handleInput1Change} />
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' type='number' value={input2} onChange={handleInput2Change} />
       
-      <p id='result'></p>
+      <p id='result'>{result}</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
